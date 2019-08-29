ebccc85cc2c19c9dc7ffc873348f430a3479508c
kholoud <kqarrot@gmail.com>
2019-08-29 18:10:05 +0300

    version2

---
+++ app/src/main/java/com/Tamazj/TamazjApp/Activity/SwitchAccountTypeActivity.java
@@ -0,0 +1,13 @@
+package com.Tamazj.TamazjApp.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SwitchAccountTypeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_switch_account_type);
+    }
+}
